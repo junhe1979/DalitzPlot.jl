192b27c7ba4b881ac8fbf0c487d7e7e6fcde5c3b
junhe1979 <109073416+junhe1979@users.noreply.github.com>
2024-01-15 21:04:51 +0800

    test

--- src/DalitzPlot.jl
+++ src/DalitzPlot.jl
@@ -641,7 +641,7 @@ function plotD(res, ch; axes=[1, 2], cg=cgrad([:white, :green, :blue, :red], [0,
     l = @layout [a _
         b{0.8w,0.8h} c]
     Plots.plot(p1, p3, p2, layout=l, titleloc=:left, titlefont=10, size=(1000, 900), left_margin=0mm, right_margin=0mm, bottom_margin=0mm, top_margin=0mm, link=:all)
-    Plots.show
+    Plots.show()
     Plots.savefig("DP.png")
 
 end
