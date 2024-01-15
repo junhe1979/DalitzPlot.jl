978498e80cf302f1b484c8b686648995bed16ee8
junhe1979 <109073416+junhe1979@users.noreply.github.com>
2024-01-15 20:40:00 +0800

    test

--- src/DalitzPlot.jl
+++ src/DalitzPlot.jl
@@ -1,7 +1,290 @@
+
 module DalitzPlot 
 
-export GENEV,Xsection, Xsection2,plotD
+export GENEV,Xsection, Xsection2,plotD,III, GA, GS, epsilon, Uc, Ubc, LCV, cdot
 using StaticArrays, ProgressBars, Distributed, Plots, LaTeXStrings, Colors, Compose, DelimitedFiles
+
+const III = SMatrix{ 4,4,ComplexF64}([
+    1.0+0.0im 0.0+0.0im 0.0+0.0im 0.0+0.0im;
+    0.0+0.0im 1.0+0.0im 0.0+0.0im 0.0+0.0im;
+    0.0+0.0im 0.0+0.0im 1.0+0.0im 0.0+0.0im;
+    0.0+0.0im 0.0+0.0im 0.0+0.0im 1.0+0.0im])
+const GA = [SMatrix{ 4,4,ComplexF64}([
+        0.0+0.0im 0.0+0.0im 0.0+0.0im 1.0+0.0im;
+        0.0+0.0im 0.0+0.0im 1.0+0.0im 0.0+0.0im;
+        0.0+0.0im -1.0+0.0im 0.0+0.0im 0.0+0.0im;
+        -1.0+0.0im 0.0+0.0im 0.0+0.0im 0.0+0.0im]),
+        SMatrix{ 4,4,ComplexF64}([
+        0.0+0.0im 0.0+0.0im 0.0+0.0im 0.0-1.0im
+        0.0+0.0im 0.0+0.0im 0.0+1.0im 0.0+0.0im
+        0.0+0.0im 0.0+1.0im 0.0+0.0im 0.0+0.0im
+        0.0-1.0im 0.0+0.0im 0.0+0.0im 0.0+0.0im]),
+        SMatrix{ 4,4,ComplexF64}([
+        0.0+0.0im 0.0+0.0im 1.0+0.0im 0.0+0.0im
+        0.0+0.0im 0.0+0.0im 0.0+0.0im -1.0+0.0im
+        -1.0+0.0im 0.0+0.0im 0.0+0.0im 0.0+0.0im
+        0.0+0.0im 1.0+0.0im 0.0+0.0im 0.0+0.0im]),
+        SMatrix{ 4,4,ComplexF64}([
+        1.0+0.0im 0.0+0.0im 0.0+0.0im 0.0+0.0im
+        0.0+0.0im 1.0+0.0im 0.0+0.0im 0.0+0.0im
+        0.0+0.0im 0.0+0.0im -1.0+0.0im 0.0+0.0im
+        0.0+0.0im 0.0+0.0im 0.0+0.0im -1.0+0.0im]),
+        SMatrix{ 4,4,ComplexF64}([
+        0.0+0.0im 0.0+0.0im 1.0+0.0im 0.0+0.0im
+        0.0+0.0im 0.0+0.0im 0.0+0.0im 1.0+0.0im
+        1.0+0.0im 0.0+0.0im 0.0+0.0im 0.0+0.0im
+        0.0+0.0im 1.0+0.0im 0.0+0.0im 0.0+0.0im])]
+
+function GS(k::SVector{5,ComplexF64})::SMatrix{4,4,ComplexF64}
+    tmp =  @MArray zeros(ComplexF64,4,4)
+    tmp[1, 1] = k[4]
+    tmp[1, 3] = -k[3]
+    tmp[1, 4] = -k[1] + im * k[2]
+    tmp[2, 2] = k[4]
+    tmp[2, 3] = -k[1] - im * k[2]
+    tmp[2, 4] = k[3]
+    tmp[3, 1] = k[3]
+    tmp[3, 2] = k[1] - im * k[2]
+    tmp[3, 3] = -k[4]
+    tmp[4, 1] = k[1] + im * k[2]
+    tmp[4, 2] = -k[3]
+    tmp[4, 4] = -k[4]
+    return tmp
+    #return GA[4] * k[4] - GA[1] * k[1] - GA[2] * k[2] - GA[3] * k[3]
+end
+
+function epsilon(k::SVector{5,Float64}, ib::Int64, l::Int64)::SVector{5,ComplexF64}
+    zm = Float64(k[5])
+    zk0 = Float64(k[4])
+    zkx = Float64(k[1])
+    zky = Float64(k[2])
+    zkz = Float64(k[3])
+    zkk = sqrt(zkx^2 + zky^2 + zkz^2)
+
+    if zkk < 1e-50
+        ct = 1.0
+        st = 0.0
+        cp = 1.0
+        sp = 0.0
+        xexpp = 1.0
+    elseif abs(zkx) < 1e-50 && abs(zky) < 1e-50
+        ct = zkz / zkk
+        st = sqrt(1.0 - ct^2)
+        cp = 1.0
+        sp = 0.0
+        xexpp = 1.0
+    else
+        ct = zkz / zkk
+        st = sqrt(1.0 - ct^2)
+        cp = zkx / zkk / st
+        sp = zky / zkk / st
+        xexpp = cp + im * sp
+    end
+
+    eps = @MArray  zeros(Complex{Float64}, 5)
+    if ib == 0
+        if l == 1
+            eps[4] = 0.0
+            eps[1] = (im * sp - ct * cp) / (xexpp * sqrt(2.0))
+            eps[2] = (-im * cp - ct * sp) / (xexpp * sqrt(2.0))
+            eps[3] = st / (xexpp * sqrt(2.0))
+        elseif l == 0
+            eps[4] = zkk / zm
+            eps[1] = zk0 / zm * st * cp
+            eps[2] = zk0 / zm * st * sp
+            eps[3] = zk0 / zm * ct
+
+        elseif l == -1
+            eps[4] = 0.0
+            eps[1] = (im * sp + ct * cp) * xexpp / (sqrt(2.0))
+            eps[2] = (-im * cp + ct * sp) * xexpp / (sqrt(2.0))
+            eps[3] = -st * xexpp / (sqrt(2.0))
+        end
+    elseif ib == 1
+        if l == 1
+            eps[4] = 0.0
+            eps[1] = (-im * sp - ct * cp) * xexpp / (sqrt(2.0))
+            eps[2] = (im * cp - ct * sp) * xexpp / (sqrt(2.0))
+            eps[3] = st * xexpp / (sqrt(2.0))
+        elseif l == 0
+            eps[4] = zkk / zm
+            eps[1] = zk0 / zm * st * cp
+            eps[2] = zk0 / zm * st * sp
+            eps[3] = zk0 / zm * ct
+        elseif l == -1
+            eps[4] = 0.0
+            eps[1] = (-im * sp + ct * cp) / (xexpp * sqrt(2.0))
+            eps[2] = (im * cp + ct * sp) / (xexpp * sqrt(2.0))
+            eps[3] = -st / (xexpp * sqrt(2.0))
+        end
+    else
+        println("wrong helicity for U")
+    end
+    return eps
+end
+
+function Uc(k::SVector{5,Float64}, l::Int64)::MVector{4,ComplexF64}
+    zkx, zky, zkz, zk0, zm = k
+    zkk::Float64 = sqrt(zkx^2 + zky^2 + zkz^2)
+    zk0m::Float64 = zk0 + zm
+    zfac::Float64 = sqrt(2.0 * zm * zk0m)
+
+    if zkk < 1e-50
+        ct = 1.0
+        st = 0.0
+        cp = 1.0
+        sp = 0.0
+        xexpp = 1.0
+        ct2 = 1.0 / zfac
+        st2 = 0.0
+    elseif abs(zkx) < 1e-50 && abs(zky) < 1e-50
+        ct = zkz / zkk
+        st = sqrt(1.0 - ct^2)
+        ct2 = sqrt((1.0 + ct) / 2.0) / zfac
+        st2 = sqrt((1.0 - ct) / 2.0) / zfac
+        cp = 1.0
+        sp = 0.0
+        xexpp = 1.0
+    else
+        ct = zkz / zkk
+        st = sqrt(1.0 - ct^2)
+        ct2 = sqrt((1.0 + ct) / 2.0) / zfac
+        st2 = sqrt((1.0 - ct) / 2.0) / zfac
+        cp = zkx / zkk / st
+        sp = zky / zkk / st
+        xexpp = cp + im * sp
+    end
+
+    U =@MVector  zeros(ComplexF64, 4)
+
+    if l == 1
+        U[1] = zk0m * ct2 / xexpp
+        U[2] = zk0m * st2
+        U[3] = zkk * ct2 / xexpp
+        U[4] = zkk * st2
+    elseif l == -1
+        U[1] = -zk0m * st2
+        U[2] = zk0m * ct2 * xexpp
+        U[3] = zkk * st2
+        U[4] = -zkk * ct2 * xexpp
+    else
+        println("wrong helicity for U")
+    end
+
+    return U
+end
+
+function Ubc(k::SVector{5,Float64}, l::Int64)::MVector{4,ComplexF64}
+    zkx, zky, zkz, zk0, zm = k
+    zkk::Float64 = sqrt(zkx^2 + zky^2 + zkz^2)
+    zk0m::Float64 = zk0 + zm
+    zfac::Float64 = sqrt(2.0 * zm * zk0m)
+
+    if zkk < 1e-50
+        ct = 1.0
+        st = 0.0
+        cp = 1.0
+        sp = 0.0
+        xexpp = 1.0
+        ct2 = 1.0 / zfac
+        st2 = 0.0
+    elseif abs(zkx) < 1e-50 && abs(zky) < 1e-50
+        ct = zkz / zkk
+        st = sqrt(1.0 - ct^2)
+        ct2 = sqrt((1.0 + ct) / 2.0) / zfac
+        st2 = sqrt((1.0 - ct) / 2.0) / zfac
+        cp = 1.0
+        sp = 0.0
+        xexpp = 1.0
+    else
+        ct = zkz / zkk
+        st = sqrt(1.0 - ct^2)
+        ct2 = sqrt((1.0 + ct) / 2.0) / zfac
+        st2 = sqrt((1.0 - ct) / 2.0) / zfac
+        cp = zkx / zkk / st
+        sp = zky / zkk / st
+        xexpp = cp + im * sp
+    end
+
+    U =@MVector  zeros(ComplexF64, 4)
+
+    if l == 1
+        U[1] = zk0m * ct2 * xexpp
+        U[2] = zk0m * st2
+        U[3] = -zkk * ct2 * xexpp
+        U[4] = -zkk * st2
+    elseif l == -1
+        U[1] = -zk0m * st2
+        U[2] = zk0m * ct2 / xexpp
+        U[3] = -zkk * st2
+        U[4] = zkk * ct2 / xexpp
+    else
+        println("wrong helicity for U")
+    end
+    return U
+end
+
+function LCV(a, b, c)::SVector{5,ComplexF64}
+    V =@MArray  zeros(ComplexF64, 5)
+    V[4] = -a[1] * b[2] * c[3] + a[1] * b[3] * c[2] + a[2] * b[1] * c[3] - a[2] * b[3] * c[1] - a[3] * b[1] * c[2] + a[3] * b[2] * c[1]
+    V[1] = -a[4] * b[2] * c[3] + a[4] * b[3] * c[2] + a[2] * b[4] * c[3] - a[2] * b[3] * c[4] - a[3] * b[4] * c[2] + a[3] * b[2] * c[4]
+    V[2] = a[4] * b[1] * c[3] - a[4] * b[3] * c[1] - a[1] * b[4] * c[3] + a[1] * b[3] * c[4] + a[3] * b[4] * c[1] - a[3] * b[1] * c[4]
+    V[3] = -a[4] * b[1] * c[2] + a[4] * b[2] * c[1] + a[1] * b[4] * c[2] - a[1] * b[2] * c[4] - a[2] * b[4] * c[1] + a[2] * b[1] * c[4]
+    return V
+end
+
+function cdot(Q::SVector{5,Float64}, W::SVector{5,Float64})::Float64
+    temp = Q[4] * W[4] - Q[1] * W[1] - Q[2] * W[2] - Q[3] * W[3]
+    return temp
+end
+
+function cdot(Q::SVector{5,Float64}, W::SVector{5,ComplexF64})::ComplexF64
+    temp = Q[4] * W[4] - Q[1] * W[1] - Q[2] * W[2] - Q[3] * W[3]
+    return temp
+end
+
+function cdot(Q::SVector{5,ComplexF64}, W::SVector{5,Float64})::ComplexF64
+    temp = Q[4] * W[4] - Q[1] * W[1] - Q[2] * W[2] - Q[3] * W[3]
+    return temp
+end
+
+function cdot(Q::SVector{5,ComplexF64}, W::SVector{5,ComplexF64})::ComplexF64
+    temp = Q[4] * W[4] - Q[1] * W[1] - Q[2] * W[2] - Q[3] * W[3]
+    return temp
+end
+
+import Base: *
+function *(A::MVector{4, ComplexF64}, B::SMatrix{4, 4, ComplexF64, 16})::MVector{4,ComplexF64}
+    temp = @MVector zeros(Complex{Float64}, 4)  # 使用Complex{Float64}来指定元素的类型
+    @inbounds for i in 1:4
+        @inbounds for j in 1:4
+            temp[i] += A[j] * B[j, i]
+        end
+    end
+    return temp
+end
+
+function *(A::MVector{4, ComplexF64}, B::MVector{4, ComplexF64})::ComplexF64
+    temp = ComplexF64(0.0)  # 使用complex(0.0, 0.0)来创建一个复数
+    @inbounds for i in 1:4
+        temp += A[i] * B[i]
+    end
+    return temp
+end
+
+function *(A::SMatrix{4, 4, ComplexF64, 16}, B::SMatrix{4, 4, ComplexF64, 16})::SMatrix{4, 4, ComplexF64, 16} 
+    C =@MArray zeros(Complex{Float64}, 4,4)   # 使用complex(0.0, 0.0)来创建一个复数
+    @inbounds for i in 1:4
+        for j in 1:4
+            for k in 1:4 
+                C[i, j] += A[i, k] * B[k, j]
+            end
+        end
+    end
+    return C
+end
+
+
 
 function cut(a::Int32)::Int64
     binary_string = bitstring(a * 69069)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,71 @@
-using DalitzPlot
+using DalitzPlot,StaticArrays
 using Test
 
 @testset "DalitzPlot.jl" begin
     
-    amp_fp2KKp(kf, ch, para)=1.
+    function amp_fp2KKp(kf::MMatrix{5,18,Float64,90}, ch::NamedTuple, para::NamedTuple)
+
+        ebm1 = sqrt(para.pW^2 + ch.p_i[1]^2)
+        tecm = sqrt((ebm1 + ch.p_i[2])^2 - para.pW^2)
+        #flux factor for cross section
+        fac = 1 / (4 * para.pW * ch.p_i[2] * (2 * pi)^5)
+        #fermion, aevrage on initial particele 
+        fac = fac * 2.0 * ch.p_i[2] * 2.0 * ch.p_f[2] / 2.0
+        #质心系到实验室系
+        GAM = (ebm1 + ch.p_i[2]) / tecm
+        ETA = para.pW / tecm
+        for i in 1:3
+            EI = kf[4, i]
+            PX = kf[1, i]
+            kf[1, i] = GAM * PX + ETA * EI
+            kf[4, i] = ETA * PX + GAM * EI
+            kf[5, i] = ch.p_f[i] #注意GENEV产生的kf[5]是三动量的大小
+        end
+    
+        l = para.l::Float64
+        p_p, p_f, p_K = ch.p_i[2], ch.p_i[1], ch.p_f[1]
+        p_rho, p_omega, p_sigma = 0.77526, 0.78626, 0.5
+        EB = 2.0 * p_K - p_f
+        # k.k
+        pf = SVector{5,Float64}([para.pW 0.0 0.0 ebm1 ch.p_i[1]]) #f0
+        pp = SVector{5,Float64}([0.0 0.0 0.0 ch.p_i[2] ch.p_i[2]]) #p
+        kKm = SVector{5,Float64}(kf[:, 1]) #K-
+        kKp = SVector{5,Float64}(kf[:, 2]) #K+
+        kp = SVector{5,Float64}(kf[:, 3]) #p
+    
+        q = pp - kp
+        q2 = cdot(q, q)
+        p = pf - kKm
+        p2 = cdot(p, p)
+    
+        # propagator with monopole form factor
+        gfKK = 5.0 #sqrt(64.0 * pi * p_f * sqrt(p_K * EB))
+        #gfKK = gfKK / (p2 - p_K^2+im*p_K*5.3169e-17)
+        gfKK = gfKK / (p2 - p_K^2 + im * p_K * 5.3169e-6)
+    
+        #aa = 1.0 / sqrt(p_K * EB)
+        #gfKK = sqrt(8.0 / p_f) * p_K * sqrt(8.0 * pi / aa) / (kKm[1]^2 + kKm[2]^2 + kKm[3]^2 + 1.0 / aa^2)
+    
+        grho = 3.02 * (-3.1) / p_rho^2 * (p_rho^2 - l^2) / (q2 - l^2)
+        gomega = -3.02 * 3.0 * (-3.1) / p_omega^2 * (p_omega^2 - l^2) / (q2 - l^2)
+        gsigma = -3.65 * 12.8 / (q2 - p_sigma^2) * (p_sigma^2 - l^2) / (q2 - l^2)
+        gq = (gfKK * (grho + gomega)) * q
+        Mq = GS(ComplexF64.(gq))
+        M = Mq + gfKK * gsigma * III
+        #M = III #Mq * Mq 
+        total0 = 0e0
+        for ilNf in -1:2:1
+            Uf = Ubc(kp, ilNf)
+            for ilNi in -1:2:1
+                Ui = Uc(pp, ilNi)
+                total0 += abs2((Uf*M*Ui)[1])
+            end
+        end
+    
+        total = total0 * fac
+    
+        return total
+    end
     p_K, p_p, p_f, p_L = 0.493677, 0.938272081, 0.987, 1.115683
     ch = (p_i=[p_f, p_p], p_f=[p_K, p_K, p_p],
         Lp_i=["f_0(980)", "p"], Lp_f=["K^-", "K^+", "p"],
@@ -15,6 +77,6 @@ using Test
 
     res=Xsection(tecm, ch,nevtot=Int64(1e6),para=(pW=pW, l=1.0))
     @show pW, res.cs0
-    plotDP.plotD(res,ch,axes=[1, 3])
+    plotD(res,ch,axes=[1, 3])
 
 end
