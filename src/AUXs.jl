8a716b06ccc8fc28fd380d5205e7fce59c82c688
junhe1979 <109073416+junhe1979@users.noreply.github.com>
2025-05-04 15:10:12 +0800

    up

--- src/AUXs.jl
+++ src/AUXs.jl
@@ -1,4 +1,5 @@
-module AUXusing Statistics
+module AUXs
+using Statistics
 # 定义一个包装函数，自动根据掩码将固定参数和自由参数合并，
 # 并提取自由参数的下界和上界
 function create_fixed_obj_from_mask(original_obj, lower, upper, full_initial, mask)
